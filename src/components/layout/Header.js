d75bb8a17521633a7bf08a59e874ff6d8cb6499a
Parth <p_patel7121996@yahoo.com>
2021-05-02 15:53:32 -0400

    installed all material icons and styled components

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,51 @@
 import React from 'react'
+import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AppsIcon from '@material-ui/icons/Apps';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
     return (
-        <div>
+        <Wrapper>
+            <LogoWrapper>
+                <div>
+                    <MenuIcon>
+
+                    </MenuIcon>
+                </div>
+                <img src='https://blog.logomyway.com/wp-content/uploads/2021/02/gmail-logo.jpg' />
+            </LogoWrapper>
+            <SearchWrapper>
+                <SearchBarWrapper>
+                    <SearchIcon />
+                    <input type= 'text' placeholder ='Search mail' />
+                    <ExpandMoreIcon />
+                </SearchBarWrapper>
+            </SearchWrapper>
+
+            <IconsWrapper>
+                <HelpOutlineIcon />
+                <SettingsIcon />
+                <AppsIcon />
+                <AccountCircleIcon />
+            </IconsWrapper>
             <h1> Hey buddy .....</h1>
-        </div>
+        </Wrapper>
     )
 }
 
 export default Header
 
+const Wrapper = styled.div``
+
+const LogoWrapper = styled.div``
+
+const SearchWrapper = styled.div``
+
+const SearchBarWrapper = styled.div``
+
+const IconsWrapper = styled.div``
