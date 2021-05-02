cd52eb4d33cb662927e94bc715a81f271b73e167
Parth <p_patel7121996@yahoo.com>
2021-05-02 17:06:52 -0400

    Added new file main.js for the mail box and side bar body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/layout/Header'
+import Main from './components/layout/Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
        
     </div>
   );
